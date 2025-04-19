dc20b84194b972a5d7a5a0f0c9084915d79ce4a7
VimalathithanNatesan <vimal.py1989@gmail.com>
2025-04-19 15:26:45 +0530

    this is a variable changes

--- variable.py
+++ variable.py
@@ -1,1 +1,10 @@
 "this is the first python program"
+"variable is a memory location, using this memory location we can store any values"
+'''find below the variable types'''
+name="vimal"
+
+"""
+here name is variable name
+= this is a assignment opreator
+"vimal" is value of the variable
+"""
