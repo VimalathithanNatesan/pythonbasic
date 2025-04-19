161c5908b444f07f487b94f5adcc88042b0c4a26
VimalathithanNatesan <vimal.py1989@gmail.com>
2025-04-19 15:41:40 +0530

    the file is updated

---
+++ datatype.py
@@ -0,0 +1,8 @@
+"this is datatype of the program"
+
+"there are diffrent type of data type available in the python program"
+1. String -"Vimala"
+2. int - 12
+3.float -11.00
+4.boolean - True or False
+5.complex - 1+4i
